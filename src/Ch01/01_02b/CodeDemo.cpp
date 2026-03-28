570e6190df96deeb4a2f5362fc34cb2ac9a9a2bd
MehradYousefi <161214659+MehradYousefi@users.noreply.github.com>
2026-03-28 23:46:59 +0000

    first code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,9 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream> // pre-processor diractives have no ; 
+int main (){
+    std::cout << "Hi there! " << std::endl;  
+    std::cout << std::endl << std::endl;   //like 2 Enter in terminal 
+    return 0;   
+}   
