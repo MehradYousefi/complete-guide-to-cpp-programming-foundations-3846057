aff13865851f24c6df8b2d4b1886d8a28b800fe3
MehradYousefi <161214659+MehradYousefi@users.noreply.github.com>
2026-05-12 20:37:23 +0000

    added a date

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03 with my modifications  
+// Exercise 00_03 with my modifications 12 May 
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
