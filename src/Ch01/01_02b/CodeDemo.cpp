128d8ec6691beca51ab4b6303f27cf405bdd3ec6
MehradYousefi <161214659+MehradYousefi@users.noreply.github.com>
2026-05-13 14:46:25 +0000

    keep going 2nd. flush

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -5,5 +5,5 @@
 int main (){
     std::cout << "Hi there! " << std::endl;  
     std::cout << std::endl << std::endl;   //like 2 Enter in terminal 
-    return 0;   
-}   
+    return 0;   // 0 indicate: no error comes in the program
+}    
--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -3,10 +3,13 @@
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
-
+#include <string> // to get string from user
 int main(){
-    std::cout << "Hi There!" << std::endl;
-
+    std::string name; // declaration
+    // to use input in same line of prompt (wit flush)
+    std::cout << "Enter your name:  " << std::flush;
+    std::cin >> name; //cin only works only for single words 
+    std::cout << "Nice to see you, " << name << "!" << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
