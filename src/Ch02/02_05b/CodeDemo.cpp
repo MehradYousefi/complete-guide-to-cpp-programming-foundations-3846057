26778360f72e58e32f332b3ede85e32939d98eea
MehradYousefi <161214659+MehradYousefi@users.noreply.github.com>
2026-05-16 22:03:23 +0000

    different variables

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -3,9 +3,26 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
-
+//same level of indentation with main fun. = global variable
+int a, b=5; //accesible to all part of code
+//best programming practice:
+    //avoid global variables
+    // use local variables as much as possible
 int main(){
-    
+    bool  my_flag; 
+    a=7; //assignment operator works rights to left
+    my_flag=false;
+    std::cout << "a= " << a << std::endl;
+    std::cout << "b= " << b << std::endl;
+    std::cout << "flag= " << my_flag << std::endl;
+    my_flag=true;
+    std::cout << "a+b= " << a+b << std::endl;
+    std::cout << "b-a= " << b-a << std::endl;
+    std::cout << "flag= " << my_flag << std::endl;
+    unsigned int positive;
+    positive=b-a; // must no have negative 
+    // value ist -2 complement respresentation in binary
+    std::cout << "b-a(unsigned)= " << positive  << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
