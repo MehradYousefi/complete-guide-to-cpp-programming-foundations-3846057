98d93e77547c01d5b135189d5f74cfefa8cdc407
MehradYousefi <161214659+MehradYousefi@users.noreply.github.com>
2026-05-17 17:46:53 +0000

    added enum class

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -1,19 +1,18 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 02_08
 // Enumerations, by Eduardo Corpeño 
-
+//we are making video games  
 #include <iostream>
 #include <cstdint>
-
-enum asset_type {texture, sound, animation, script};
+//texture 0 , sound 1 , animation 2, script 3
+enum class asset_type {texture, sound, animation, script};
+enum class menu_section {background, music, sound, controls, texture};
 
 int main(){
-    int asset_value;
-
-    asset_value = sound;
-
-    std::cout << "asset_value = " << asset_value << std::endl;
-
+    asset_type asset_value;
+    asset_value = asset_type::sound;
+    //cast asset<-value to int
+    std::cout << "asset_value = " << (int) asset_value << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
