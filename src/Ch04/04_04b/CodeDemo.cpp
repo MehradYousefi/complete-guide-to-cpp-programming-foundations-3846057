1bd0a59cc63160867b60212217a4272917692505
MehradYousefi <161214659+MehradYousefi@users.noreply.github.com>
2026-05-25 19:19:34 +0000

    dynamic memory with pointers

--- src/Ch04/04_04b/CodeDemo.cpp
+++ src/Ch04/04_04b/CodeDemo.cpp
@@ -4,28 +4,34 @@
 
 #include <iostream>
 #include <string>
-#include <chrono>
+#include <chrono>   
 
-// Type aliases for chrono classes
-using Clock = std::chrono::system_clock;
+using Clock = std::chrono::system_clock; 
 using TimePoint = std::chrono::time_point<Clock>;
 using Duration = std::chrono::duration<double>;
 
 int main(){
-    TimePoint start, end;
-    Duration elapsed_seconds;
+    TimePoint *start, *end; 
+    //for object we allocate memory with new opeator and constructor
+    Duration *elapsed_seconds= new Duration() ; 
     std::string input;
 
-    start = Clock::now();
-
+    start = new TimePoint (Clock::now()) ; 
+    
     std::cout << "Type 'start' as quickly as you can and hit ENTER: " << std::flush;
     std::cin >> input;
 
-    end = Clock::now();
-    elapsed_seconds = end - start;
-
-    std::cout << "Reaction time: " << elapsed_seconds.count() << "s" << std::endl;
+    end = new TimePoint  (Clock::now());
+    *elapsed_seconds = *end - * start;
+    std::cout << "Reaction time: " << elapsed_seconds->count() << "s" << std::endl;
+    // -> is member of pointer operator  pointer -> member
     
+    //objects dynamically created now delete them
+    // delete operator calls destructor classes 
+    delete start;
+    delete end; 
+    delete elapsed_seconds; 
+     
     std::cout << std::endl << std::endl;
     return 0;
 }
